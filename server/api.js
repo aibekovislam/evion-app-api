1f55d72541a079bca296014b0eca79e35feeb577
Islam Aibekov <islamaibekov@Islams-MacBook-Air.local>
2023-09-14 08:24:20 +0600

    first commit

---
+++ server/api.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+
+const app = express();
+const port = 4000;
+
+mongoose.connect('mongodb+srv://islamxdaamduu:200x30003x@cluster0.o882hth.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const User = mongoose.model('User', {
+  username: String,
+  phone: String,
+  password: String,
+});
+
+app.use(bodyParser.json());
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, phone, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, phone, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Registration failed' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+    const user = await User.findOne({ phone });
+    if (!user) {
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
+    const token = jwt.sign({ userId: user._id }, 'secret-key', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Authentication failed' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
