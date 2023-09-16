44c9aeca15b65a7ffa2f0c6dfa2b7801564b7526
aibekovislam <islamxdaamduu@gmail.com>
2023-09-16 12:11:37 +0600

    changed token

--- server/api.js
+++ server/api.js
@@ -127,7 +127,7 @@ app.post('/login', async (req, res) => {
 
 app.get('/profile', async (req, res) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(' ')[1];
 
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
